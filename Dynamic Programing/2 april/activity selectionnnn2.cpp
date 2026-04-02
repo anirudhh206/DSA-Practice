b47a2e2a25958281ab8f040b81e1b5b6484aa7b9
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-04-03 01:49:55 +0530

    Practicing

--- Dynamic Programing/2 april/activity selectionnnn2.cpp
+++ Dynamic Programing/2 april/activity selectionnnn2.cpp
@@ -8,7 +8,23 @@ int main(){
     vector<int> start;
     vector<int>end;
 
+    for(int i=0;i<n;i++)cin>>start[i];
+    for(int i=0;i<n;i++)cin>.end[i];
+
+    vector<pair<int,int>>activities;
     for(int i=0;i<n;i++){
-        
+        activities.push_back({start[i],end[i]});
+    }
+    sort(activities.begin(),activities.end(),[](auto &a, auto &b){return a[1]<b[1]});
+
+    int count=0;
+    int last_end=INT_MIN;
+    for(auto &act:activities){
+        int s=act[0];
+        int e=act[1];
+        if(s > last_end){
+            count++;
+            last_end=e;
+        }
     }
 }
