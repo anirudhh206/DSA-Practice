42e0736e5cf94d31f9c287b37774ce4b4af6b41e
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-04-03 01:52:02 +0530

    Practicing

--- Dynamic Programing/2 april/activity selectionnnn2.cpp
+++ Dynamic Programing/2 april/activity selectionnnn2.cpp
@@ -5,26 +5,27 @@ int main(){
     int n;
     cin>>n;
 
-    vector<int> start;
-    vector<int>end;
+    vector<int> start(n);
+    vector<int>end(n);
 
     for(int i=0;i<n;i++)cin>>start[i];
-    for(int i=0;i<n;i++)cin>.end[i];
+    for(int i=0;i<n;i++)cin>>end[i];
 
     vector<pair<int,int>>activities;
     for(int i=0;i<n;i++){
         activities.push_back({start[i],end[i]});
     }
-    sort(activities.begin(),activities.end(),[](auto &a, auto &b){return a[1]<b[1]});
+    sort(activities.begin(),activities.end(),[](auto &a, auto &b){return a.second<b.second});
 
     int count=0;
     int last_end=INT_MIN;
     for(auto &act:activities){
-        int s=act[0];
-        int e=act[1];
+        int s=act.first;
+        int e=act.second;
         if(s > last_end){
             count++;
             last_end=e;
         }
     }
+    cout << count;
 }
