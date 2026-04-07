233e696b520f1018285055e697c85813c8038b4f
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-04-07 22:31:44 +0530

    Practicing

--- Dynamic Programing/2 april/job sequencing .cpp
+++ Dynamic Programing/2 april/job sequencing .cpp
@@ -1,38 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-struct job{
+
+struct job {
     int id, deadline, profit;
 };
 
-bool cmp(job a, job b){
-    return a.profit>b.profit;
+bool cmp(job a, job b) {
+    return a.profit > b.profit;
 }
 
-int main(){
+int main() {
     int n;
-    cin>>n;
+    cin >> n;
     vector<job> jobs(n);
-    for(int i=0;i<n;i++){
-        cin>>jobs[i].id >>jobs[i].deadline >> jobs[i].profit;
+    for (int i = 0; i < n; i++) {
+        cin >> jobs[i].id >> jobs[i].deadline >> jobs[i].profit;
     }
+
     sort(jobs.begin(), jobs.end(), cmp);
 
-    int maxDeadline= 0;
-    for(auto &job:jobs){
-        maxDeadline= max(maxDeadline, job.deadline);
+    int maxDeadline = 0;
+    for (auto &job : jobs) {
+        maxDeadline = max(maxDeadline, job.deadline);
     }
-    vector<int> slot(maxDeadline+1,-1);
-    int count=0, totalprofit=0;
 
-    for(auto &job:jobs){
-        for(int j=job.deadline;j>0;j--){
-            if(slot[j]==-1){
-                slot[j]=job.id;
+    vector<int> slot(maxDeadline + 1, -1);
+    int count = 0, totalProfit = 0; // fixed variable name
+
+    for (auto &job : jobs) {
+        for (int j = job.deadline; j > 0; j--) {
+            if (slot[j] == -1) {
+                slot[j] = job.id;
                 count++;
-                totalProfit += job.profit;
+                totalProfit += job.profit; // consistent usage
                 break;
             }
         }
     }
+
     cout << count << " " << totalProfit;
 }
