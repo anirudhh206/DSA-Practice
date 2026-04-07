8ca2dfe9955857c8bbed59ed5c1546a8a8a6e7a6
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-04-07 22:57:29 +0530

    Practicing

---
+++ Dynamic Programing/7 April/job scheduling.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct job{
+    int id, deadline, profit;
+};
+
+bool cmp(job a, job b){
+    return a.profit>b.profit;
+}
+
+int main(){
+    int n; cin>>n;
+    vector<job> jobs(n);
+    for(int i=0;i<n;i++){
+        cin>>jobs[i].id>>jobs[i].deadline>>jobs[i].profit;
+    }
+
+    sort(jobs.begin(), jobs.end(), cmp);
+
+    int maxDeadline=0;
+    for(auto &job :jobs){
+        maxDeadline= max(maxDeadline, job.deadline);
+    }
+
+    vector<int> slot(maxDeadline+1, -1);
+    int count=0, int totalprofit=0;
+
+    for(auto &job: jobs){
+        for(int i=job.deadline; j>0;j--){
+            if(slot[i]==-1){
+                slot[i]=job.id;
+                count++;
+                totalprofit+= job.profit;
+                break;
+            }
+        }
+    }
+    cout<<count<<" "<<totalprofit;
+}
