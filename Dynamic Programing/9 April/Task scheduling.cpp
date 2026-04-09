2467253de89464c37469079064eb824866ef97df
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-04-09 23:31:21 +0530

    Practicing

--- Dynamic Programing/9 April/Task scheduling.cpp
+++ Dynamic Programing/9 April/Task scheduling.cpp
@@ -1,6 +1,33 @@
+
 #include<bits/stdc++.h>
 using namespace std;
 
 int minimumRounds(vector<int> &tasks){
-    
+    unordered_map<int,int> freq;
+    for(int t:tasks){
+        freq[t]++;
+    }
+    int totalRounds =0;
+    for(auto it : freq){
+        int f = it.second;
+        if(f==1)return -1;
+         
+        totalRounds += f/3;
+        if(f%3 != 0){
+            totalRounds += 1;
+        }
+    }
+    return totalRounds;
+}
+
+int main(){
+    int n;
+    cin>> n;
+    vector<int>tasks(n);
+    for(int i=0;i<n;i++){
+        cin>>tasks[i];
+    }
+
+    cout<< minimumRounds(tasks);
+
 }
