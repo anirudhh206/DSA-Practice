4731f1f3576ed56a6a160ea819158cb8f0169289
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-04-11 12:45:27 +0530

    Practicing

---
+++ Dynamic Programing/11 april/matrix chain .cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+
+    vector<vector<int>> dp(n,vector<int>(n,0));
+
+    for(int len=0;len<n;len++){
+        for(int i=0i<n-len+1;i++){
+            int j= i+len-1;
+            dp[i][j]= INT_MAX;
+            for(int k=i;k<j;k++){
+                int cost = dp[i][k]+dp[k+1][j]+arr[i-1]*arr[k]*arr[j];
+                dp[i][j]= min(dp[i][j],cost);
+            }
+        }
+    }
+    cout<<dp[1][n-1];
+}
