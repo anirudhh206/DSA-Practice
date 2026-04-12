f270e9e5593edb39a0ac80c53710684bcf36d590
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-04-12 16:26:16 +0530

    Practicing

--- Dynamic Programing/11 april/all pair shortest path.cpp
+++ Dynamic Programing/11 april/all pair shortest path.cpp
@@ -17,8 +17,25 @@ int main(){
     for(int k=0;k<n;k++){
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(dist[i][k] != INF && dist[k][j] != INF)
+                if(dist[i][k] != INF && dist[k][j] != INF){
+                    dist[i][j]= min(dist[i][j],
+                    dist[i][k]+dist[k][j]);
+                }
             }
         }
     }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(dist[i][j]==INF){
+                cout<<"INF";
+            }
+            else{
+                cout<<dist[i][j]<<" ";
+            }
+
+        }
+        cout<<endl;
+    }
+    return 0;
 }
