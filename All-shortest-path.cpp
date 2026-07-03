3ae9f1f1ddcbe95eade4c7a5841d74f8a476ec73
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-07-04 01:53:01 +0530

    Learning the All shortest path

---
+++ All-shortest-path.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int INF=1e9;
+int main(){
+    int n;
+    cin>>n;
+    vector<vector<int>> dist(n, vector<int>(n));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>dist[i][j];
+            if(dist[i][j]==-1 && i!=j)dist[i][j]=INF;
+        }
+    }
+    for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j =0;j<n;j++){
+                if(dist[i][k]<INF && dist[k][j]<INF){
+                    dist[i][j]=min(dist[i][j], dist[i][k]+dist[k][j]);
+                }
+            }
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            fi(dist[i][j]==INF)cout<<"INF";
+            else cout<<dist[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
