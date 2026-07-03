d45b21820c4da927ded7d7616346a4225f86fb66
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-07-04 02:57:13 +0530

    Learning

---
+++ sum-increasing-subsequencing.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int>dp(n);
+    for(int i=0;i<n;i++){
+        dp[i]=arr[i];
+    }
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if(arr[i]>arr[j]){
+                dp[i]=max(dp[i],dp[j]+arr[i]);
+            }
+        }
+    }
+    int ans=0;
+    for(int i=0;i<n;i++){
+        ans=max(ans,dp[i]);
+    }
+    cout<<ans<<endl;
+}
