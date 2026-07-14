8aeef1d3f86cb004522cc560449d82e58d7bbece
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-07-15 00:47:51 +0530

    Learning DSA

--- krush.cpp
+++ krush.cpp
@@ -41,6 +41,48 @@ public:
         }
         return true;
     }
-} struct edge
+} struct edges
 {
+    int u, v, w;
+};
+int main()
+{
+    int V, E;
+    cin >> V >> E;
+    vector<edges> edges(E);
+    for (int i = 0; i < E; i++)
+    {
+        cin >> edges[i].u >> edges[i].v >> edges[i].w;
+    }
+    sort(edges.begin(), edges.end(), [](edges a, edges b)
+         { return a.w < b.w; });
+    DSU dsu(v);
+    int totalCost = 0;
+    vector<edges> mst;
+    for (auto edge : edges)
+    {
+
+        if (dsu.unite(edge.u, edge.v))
+        {
+            mst.push_back(edge);
+            totalCost += edge.w;
+        }
+
+        if (mst.size() == V - 1)
+            break;
+    }
+
+    // Print MST edges
+    for (auto edge : mst)
+    {
+        cout << edge.u << " "
+             << edge.v << " "
+             << edge.w << endl;
+    }
+
+    // Print total cost
+    cout << totalCost << endl;
+
+    return 0;
+}
 }
