b4ee4d4627705694348a9576a5f71360a2830c69
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-07-14 13:16:02 +0530

    Learning Job sequencing

--- Job sequencing.cpp
+++ Job sequencing.cpp
@@ -1,19 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-int main(){
+struct job
+{
+    int id,
+        int deadline,
+        int profit,
+};
+bool compare(job a, job b)
+{
+    return a.profit > b.profit;
+}
+int main()
+{
     int n;
-    cin>>n;
-    vector<job>jobs(n);
-    for(int i=0;i<n;i++){
-        cin>>jobs[i].id>>jobs[i].deadline>>jobs[i].profit;
+    cin >> n;
+    vector<job> jobs(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> jobs[i].id >> jobs[i].deadline >> jobs[i].profit;
     }
-    sort(job.begin(),job.end(),compare);
-
-    int maxdeadline=0;
-    for(auto &job:jobs){
-        maxDeadline=max(maxdeadline, job.deadline);
+    int maxdeadline = 0;
+    for (auto &job : jobs)
+    {
+        maxdeadline = max(maxdeadline, job.profit);
     }
-
-    
+    vector<int> slot(maxdeadline + 1, -1);
+    int count = 0, tota_profit = 0;
+    for (auto &job : jobs)
+    {
+        for (int i = 0; i < job.deadline; i++)
+        {
+            if (slot[i] == -1)
+            {
+                slot[i] = job.id;
+                count++;
+                total_profit += job.profit;
+                break;
+            }
+        }
+    }
+    cout << count << " " << total_profit;
 }
