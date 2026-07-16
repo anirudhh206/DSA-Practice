3325775e73344eed54a604a68179f96977eabffb
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-07-16 06:41:30 +0530

    Learning DSA

---
+++ jobsequen.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct job
+{
+    int id, deadline, profit;
+};
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<job> job;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> job[i].id >> job.[i].deadline >> job[i].profit;
+    }
+}
