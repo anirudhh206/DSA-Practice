02a079e3fd9fba8b8098da3009bbbb21ce14117f
anirudhh206 <anirudhvashisth2006@gmail.com>
2026-07-16 06:55:12 +0530

    Learning DSA

--- jobsequen.cpp
+++ jobsequen.cpp
@@ -5,7 +5,10 @@ struct job
 {
     int id, deadline, profit;
 };
-
+bool compare(job a, job b)
+{
+    return a.profit > b.profit;
+}
 int main()
 {
     int n;
@@ -15,4 +18,26 @@ int main()
     {
         cin >> job[i].id >> job.[i].deadline >> job[i].profit;
     }
+    sort(job.begin(), job.end(), compare);
+    int maxdeadline = 0;
+    for (auto &job : job)
+    {
+        maxdeadline = max(maxdeadline, job.deadline);
+    }
+    int count = 0;
+    vector<int> slot(maxdeadline + 1, -1);
+    for (auto &job : job)
+    {
+        for (int i = job.deadline; i > 0; i--)
+        {
+            if (slot[i] == 1)
+            {
+                slot[j] = job.id;
+                count++;
+                totalprofit += job.profit;
+                break;
+            }
+        }
+    }
+    cout << count << " " << totalprofit
 }
